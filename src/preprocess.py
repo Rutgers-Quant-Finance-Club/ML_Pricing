8757eebdea0d1e41a1918cae88ce84a2acc2e4f4
lawho13 <141877167+lawho13@users.noreply.github.com>
2026-03-08 16:20:47 -0400

    Update preprocess.py
    cross sectional median imputation, dropping remaining NA leaving completed stock characteristics from 1985-2021.

--- src/preprocess.py
+++ src/preprocess.py
@@ -10,7 +10,24 @@ class DataPipeline:
     def load(self):
         self.df = pd.read_csv(self.filepath)
         print(f"Loaded {self.df.shape[0]} rows and {self.df.shape[1]} columns")
+        self.df['DATE'] = pd.to_datetime(df['DATE'], errors='coerce', format = '%Y%m%d')
+        # Company identified by PERMNO Number
+        # Date YYYYMMDD
+        self.df['year_month'] = self.df['DATE'].dt.to_period('M')
+        # Keeping track of year and month
         return self
 
     def impute_missing(self):
+        num_cols = self.df.select_dtypes(include='number').columns
+        self.df[num_cols] = self.df[num_cols].fillna(
+            self.df.groupby('year_month')[num_cols].transform('median')
+        )
+        # imputation of numeric columns using median of the respective month/year
+        # following the direction of original paper
+        # no non-numeric columns upon further investigation
+        cleaned = self.df.dropna()
+        
+        print(f"Cleaned Data: {cleaned.df.shape[0]} rows and {cleaned.df.shape[1]} columns")
+        self.df=cleaned
+        
         return self
